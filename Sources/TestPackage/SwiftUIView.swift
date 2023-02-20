203941541c156eff4e0b4f747bef63d9de4f102b
honey193 <honeyben.shah@zopsmart.com>
2023-02-20 12:54:31 +0530

    update

--- Sources/TestPackage/SwiftUIView.swift
+++ Sources/TestPackage/SwiftUIView.swift
@@ -9,22 +9,27 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 public struct SwiftUIView: View {
-    @State public var showingAlert = true
-    public var alertMessage: String
-    public var buttonText: String
-    public init(showingAlert: Bool = true, alertMessage: String, buttonText: String) {
-        self.showingAlert = showingAlert
+    @State var showingAlert = true
+    var alertMessage: String?
+    var buttonText: String?
+    var buttonClick: () -> Void
+    
+    init(alertMessage: String, buttonText: String, buttonClick: @escaping () -> Void) {
+        self.buttonClick = buttonClick
         self.alertMessage = alertMessage
         self.buttonText = buttonText
     }
+    
     public var body: some View {
         if #available(iOS 15.0, *) {
             ZStack { }
-                .alert(alertMessage, isPresented: $showingAlert) {
-                    Button(buttonText, role: .cancel) { }
+                .alert(alertMessage ?? "", isPresented: $showingAlert) {
+                    Button(buttonText ?? "", role: .cancel, action: {
+                        buttonClick()
+                    })
                 }
         } else {
-            Text(alertMessage)
+            Text(alertMessage ?? "")
         }
     }
 }
