883fd524f4dc634b5f6e058bdc98094d5101c70e
honey193 <honeyben.shah@zopsmart.com>
2023-02-20 12:18:50 +0530

    update

--- Sources/TestPackage/SwiftUIView.swift
+++ Sources/TestPackage/SwiftUIView.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 @available(iOS 13.0, *)
-struct SwiftUIView: View {
-    @State var showingAlert = true
-    var alertMessage: String
-    var buttonText: String
-    var body: some View {
+public struct SwiftUIView: View {
+    @State public var showingAlert = true
+    public var alertMessage: String
+    public var buttonText: String
+    public var body: some View {
         if #available(iOS 15.0, *) {
             ZStack { }
                 .alert(alertMessage, isPresented: $showingAlert) {
